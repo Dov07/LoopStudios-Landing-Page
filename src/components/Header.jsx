7174af9f0c2955e48ad865be6122b668fb8a9211
dov07 <olcayvural7@gmail.com>
2026-05-08 02:02:17 +0300

    adding web responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { useToggle } from '@uidotdev/usehooks'
+import React, { useEffect } from 'react'
+import { useToggle, useWindowSize } from '@uidotdev/usehooks'
 import logo from '../assets/iconPhotos/logo.svg'
 import hamburger from '../assets/iconPhotos/icon-hamburger.svg'
 import closeIcon from '../assets/iconPhotos/icon-close.svg'
 import bgImg from '../assets/image-hero.jpg'
+import bgWeb from '../assets/image-hero-w.jpg'
 import '../Css/Header.css'
 const Header = ({ Navlink }) => {
 
 
   const [isMenuOpen, toggleMenu] = useToggle()
+  const { width } = useWindowSize()
+
+  useEffect(() => {
+    if (width > 768 && isMenuOpen) {
+      toggleMenu(false) // Ekran 768px'den büyükse ve menü açıksa menüyü kapat
+    }
+  }, [width, isMenuOpen, toggleMenu])
 
   return (
     <div className='header-wrapper'>
       <div className='background-img'>
 
-        <img src={bgImg} alt="" />
+        <img src={width >= 768 ? bgWeb : bgImg} alt="Hero Background" />
       </div>
       <header className="header">
 
@@ -25,11 +33,9 @@ const Header = ({ Navlink }) => {
           </a>
           
         </div>
-        <div>
-          <button className="hamburger-btn" onClick={() => toggleMenu()} aria-expanded={isMenuOpen} aria-label="Toggle menu">
-            <img src={isMenuOpen ? closeIcon : hamburger} alt={isMenuOpen ? "Menüyü kapat" : "Menüyü aç"} />
-          </button>
-        </div>
+        <button className="hamburger-btn" onClick={() => toggleMenu()} aria-expanded={isMenuOpen} aria-label="Toggle menu">
+          <img src={isMenuOpen ? closeIcon : hamburger} alt={isMenuOpen ? "Menüyü kapat" : "Menüyü aç"} />
+        </button>
 
 
         <nav className={` ${isMenuOpen? "open" :"" }`}>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import '../Css/Main.css'
 import { gallery } from '../gallery'
+import { useWindowSize } from '@uidotdev/usehooks'
 import heroHype from '../assets/image-interactive.jpg'
+import heroWeb from '../assets/image-interactive-w.jpg'
 const Main = () => {
+
+  const { width } = useWindowSize()
   return (
     <main>
       <div className='top-hero-img'>
-        <img src={heroHype} alt="" />
+        <img src={width >= 768 ? heroWeb : heroHype} alt="" />
         <div className='top-hero-desc'>
-          <h2>The Leader In Interactive VR</h2>
-          <p>Founded in 2011 loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital expreiences that bind to their brand.</p>
+          <div className='top-hero-desc-wrapper'>
+            <h2>The Leader In Interactive VR</h2>
+            <p>Founded in 2011 loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital expreiences that bind to their brand.</p>
+          </div>
         </div>
-
       </div>
       <div className='bottom-gallery'>
         <h2>
@@ -20,14 +25,14 @@ const Main = () => {
         <div className='gallery'>
           {gallery.map((item) => (
             <div className='gallery-wrapper' key={item.id}>
-              <img src={item.mobileImg} alt={item.alt} />
+              <img src={width >= 768 ? item.webImg :  item.mobileImg} alt={item.alt} />
               <p>{item.alt}</p>
             </div>
           ))}
         </div>
-        
-          <button className='seeAll'>See ALL</button>
-        
+
+        <button className='seeAll'>See ALL</button>
+
       </div>
     </main>
   )
