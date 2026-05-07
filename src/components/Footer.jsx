390ae7834fe33ce293b95b9553e336a567a75940
dov07 <olcayvural7@gmail.com>
2026-05-08 02:39:06 +0300

    genel görünüm tamamlandı devamında hover ve responsive çöküşüne breakpoint eklenecek

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,35 +9,36 @@ import twitterIcon from '../assets/iconPhotos/icon-twitter.svg'
 
 const Footer = ({ Navlink }) => {
 
-const links =  [facebookIcon,instagramIcon,pinterestIcon,twitterIcon]
+  const links = [facebookIcon, instagramIcon, pinterestIcon, twitterIcon]
 
   return (
     <footer>
-      <div className='footer-top'>
-        <a href="/">
-        <img src={logo} alt="logo" />
-        </a>
-        <div className='nav-link-footer'>
-          <ul>
-            {Navlink.map((e) => (
+        <div className='footer-top'>
+          <a href="/">
+            <img src={logo} alt="logo" />
+          </a>
+          <div className='nav-link-footer'>
+            <ul>
+              {Navlink.map((e) => (
 
-              <li key={e}>
-                <a href="">{e}</a>
-              </li>
+                <li key={e}>
+                  <a href="">{e}</a>
+                </li>
 
-            ))}
-          </ul>
-        </div>
-      </div>
-      <div className='social-links'>
-           
-              {links.map((e)=>(
-                <a key={e} href="#">
-                  <img src={e} alt="link" />
-                </a>
               ))}
-            
-      </div>
+            </ul>
+          </div>
+        </div>
+        <div className='social-links'>
+
+          {links.map((e) => (
+            <a key={e} href="#">
+              <img src={e} alt="link" />
+            </a>
+          ))}
+
+        </div>
+      
       <p className='footer-bottom-p'>2021 Loopstudios. All rights reserved.</p>
     </footer>
   )
